26ad450c2b2eb06efa90df9e72b04712ef25bc2e
HAN <dhhan345@gmail.com>
2024-04-08 11:01:13 +0900

    TEST

---
+++ src/main/java/data/dto/dbTestDto.java
@@ -0,0 +1,29 @@
+package data.dto;
+
+import java.security.Timestamp;
+
+public class dbTestDto {
+	private String num;
+	private String content;
+	private Timestamp writeday;
+	
+	public String getNum() {
+		return num;
+	}
+	public void setNum(String num) {
+		this.num = num;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Timestamp getWriteday() {
+		return writeday;
+	}
+	public void setWriteday(Timestamp writeday) {
+		this.writeday = writeday;
+	}
+
+}
